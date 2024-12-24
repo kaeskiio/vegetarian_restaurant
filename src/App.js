6d60d0282c520c35719abbe2f78b4965fd9e30b2
Joshua <joshwu10806@gmail.com>
2024-12-24 14:44:51 -0600

    add Fade in on scroll

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { AboutUs, Chef, FindUs, FoodofDay, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    <AboutUs />
-    <FoodofDay />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
-  </div>
-);
+
+const App = () => {
+
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <FoodofDay/>
+      <SpecialMenu />
+      <Chef />
+      <Intro />
+      <Laurels />
+      <Gallery />
+      <FindUs />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,14 +3,21 @@ import React from 'react';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
+import FadeInSection from '../../components/FadeInSection/FadeInSection';
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavor" />
-      <h1 className="app__header-h1">Guilt-free Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Fresh produce from our fully organic farms in Scarsborough, New York. Satisfy your cravings with our sustainable yet scrumptious creations. Book your reservation today!</p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <FadeInSection>
+        <SubHeading title="Chase the new flavor" />
+      </FadeInSection>
+      <FadeInSection>
+        <h1 className="app__header-h1">Guilt-free Fine Dining</h1>
+      </FadeInSection>
+      <FadeInSection>
+        <p className="p__opensans" style={{ margin: '2rem 0' }}>Fresh produce from our fully organic farms in Scarsborough, New York. Satisfy your cravings with our sustainable yet scrumptious creations. Book your reservation today!</p>
+        <button type="button" className="custom__button">Explore Menu</button>
+      </FadeInSection>
     </div>
     <div className="app__wrapper_img">
       <img src={images.header_image} alt="header_img" />
