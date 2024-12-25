c398f36fd064deaedb9942f6dc6a96a3857b28c8
Joshua <joshwu10806@gmail.com>
2024-12-25 12:44:29 -0600

    add negroni picture and new tastingmenu component

---
+++ src/container/TastingMenu/TastingMenu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {SubHeading, MenuItem } from '../../components'
+import './TastingMenu.css';
+
+const TastingMenu = () => {
+  <div className = ".app__tastingmenu flex__center section__padding">
+    <div className = ".app__tastingmenu-chef">
+      <div classnName = ".app__tastingmenu-title">
+        <h1 className="headtext__cormorant">Chef's Tasting Menu</h1>
+        <SubHeading>Made with fresh ingredients from the farm</SubHeading>
+      </div>
+      <div className=".app__tastingmenu-">
+
+      </div>
+    </div>
+  </div>
+};
+
+export default TastingMenu;
