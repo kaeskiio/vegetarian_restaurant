eacce223b2ecd5cbcfda6fcaff84fb80c1ed8ae4
Kacem Ettahali <101kacem101@gmail.com>
2024-12-24 19:55:48 -0600

    Fix FadeIn Layout Issues

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -11,25 +11,29 @@ const AboutUs = () => (
       <img src={images.G} alt="G_overlay" />
     </div>
     <div className="app__aboutus-content flex__center">
-      <FadeInSection translateXAmount ={-30}>
-        <div className="app__aboutus-content_about" translateXAmount={-30}>
+      <div className="app__aboutus-content_wrapper">
+        <FadeInSection translateXAmount={-30}>
+          <div className="app__aboutus-content_about">
             <h1 className="headtext__cormorant">About Us</h1>
             <img src={images.spoon} alt="about_spoon" className="spoon__img" />
             <p className="p__opensans">At Black Basil, we seek to promote a sustainable community through innovative approaches to fine dining. Featuring an entirely plant-based menu, we hope to build a better future for humanity one meal at a time.</p>
             <button type="button" className="custom__button">Know More</button>
-        </div>
-      </FadeInSection>
+          </div>
+        </FadeInSection>
+      </div>
       <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="about_knife" />
       </div>
-      <FadeInSection translateXAmount={30}>
-        <div className="app__aboutus-content_history" translateXAmount={30}>
-          <h1 className="headtext__cormorant">Our History</h1>
-          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-          <p className="p__opensans">Black Basil was founded by Executive Chef Kacem Ettahali and Head Chef Joshua Wu in 2020. During the Covid 19 Pandemic, Black Basil was created as an attempt to encourage a sustainable living that would support our planet. After a year of careful experimentation with plant-based menus, Black Basil had its grand opening in the fall of 2020.</p>
-          <button type="button" className="custom__button">Know More</button>
-        </div>
-      </FadeInSection>
+      <div className="app__aboutus-content_wrapper">
+        <FadeInSection translateXAmount={30}>
+          <div className="app__aboutus-content_history">
+            <h1 className="headtext__cormorant">Our History</h1>
+            <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+            <p className="p__opensans">Black Basil was founded by Executive Chef Kacem Ettahali and Head Chef Joshua Wu in 2020. During the Covid 19 Pandemic, Black Basil was created as an attempt to encourage a sustainable living that would support our planet. After a year of careful experimentation with plant-based menus, Black Basil had its grand opening in the fall of 2020.</p>
+            <button type="button" className="custom__button">Know More</button>
+          </div>
+        </FadeInSection>
+      </div>
     </div>
   </div>
 );
